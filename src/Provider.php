c19ec5366637773e1eb125b46325093dc6e2c7a1
saasfreelancer <syed.sayal.shah@gmail.com>
2023-02-20 16:04:14 +0500

    chore: update author, copyright

--- src/Provider.php
+++ src/Provider.php
@@ -5,10 +5,11 @@
  * This is the service provider for the language system, which binds an instance
  * of the framework's `Language` class to the container.
  *
- * @package   HybridCore
- * @author    Justin Tadlock <justintadlock@gmail.com>
- * @copyright Copyright (c) 2008 - 2021, Justin Tadlock
- * @link      https://themehybrid.com/hybrid-core
+ * @package   HybridLang
+ * @link      https://github.com/themehybrid/hybrid-lang
+ *
+ * @author    Theme Hybrid
+ * @copyright Copyright (c) 2008 - 2023, Theme Hybrid
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
